0cc77bb88e2121464aff780c024b27f34dde35af
Matt Miller <matt.f.miller@live.com>
2018-05-25 09:10:42 -0700

    Add and pass test for instantiating event object.

--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -1,5 +1,18 @@
 package models;
 
 public class Event {
+    private double price;
+    private int guests;
+    private String food;
+    private String beverage;
+    private String entertainment;
+
+    public Event() {
+        this.price = 0.00;
+        this.guests = 0;
+        this.food = "none";
+        this.beverage = "none";
+        this.entertainment = "none";
+    }
 
 }
--- src/test/java/models/EventTest.java
+++ src/test/java/models/EventTest.java
@@ -1,7 +1,13 @@
 package models;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class EventTest {
-
+    @Test
+    public void newEvent_instantiatesCorrectly() throws Exception {
+        Event testEvent = new Event();
+        assertTrue(testEvent instanceof Event);
+    }
 }
