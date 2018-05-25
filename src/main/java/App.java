b80283986793e3cde14463d1c6e69e7af7146430
Matt Miller <matt.f.miller@live.com>
2018-05-25 15:02:35 -0700

    Add user logic to collect food, drink, entertainment and coupon code.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,7 +15,24 @@ public class App {
                 System.out.println("How many guests will be attending the party?");
                 String userGuests = bufferedReader.readLine();
                 event.setGuests(Integer.parseInt(userGuests));
-                System.out.println("How many guests will be attending the party?");
+                System.out.println("What kind of food would you like to have served at your party? snacks, dinner, dinner with dessert, or none");
+                String userFood = bufferedReader.readLine();
+                event.setFood(userFood.toLowerCase());
+                System.out.println("What kind of beverages would you like to have served at your party? punch, full bar, or none");
+                String userDrink = bufferedReader.readLine();
+                event.setDrink(userDrink.toLowerCase());
+                System.out.println("What kind of entertainment would you like to have  at your party? DJ, band, or none");
+                String userEntertainment = bufferedReader.readLine();
+                event.setEntertainment(userEntertainment.toLowerCase());
+                System.out.println("Do you have a coupon code you would like to apply to this event? y/n");
+                String userHasCoupon = bufferedReader.readLine();
+                if (userHasCoupon.equals("y")){
+                    System.out.println("Go ahead and enter your coupon code now");
+                    String userCoupon = bufferedReader.readLine();
+                    event.setCouponCode(userCoupon);
+                } else {
+
+                }
 
             } catch (IOException e) {
                 e.printStackTrace();
